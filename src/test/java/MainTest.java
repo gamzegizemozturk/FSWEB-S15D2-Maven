bd5dae8688c7b01d26d8b1359d63a0c52190b347
gamzegizemozturk <gamzegizemoztuurk@gmail.com>
2025-07-28 23:41:13 +0300

    done

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -130,7 +131,7 @@ public class MainTest {
         Set<Task> taskSet2 = new HashSet<>();
         taskSet2.add(task2);
 
-        Set<Task> differences = taskData.getDifferences(taskSet, taskSet2);
+        Set<Task> differences = taskData.getDifference(taskSet, taskSet2);
 
         for(Task task: differences){
             assertEquals(task, task1);
